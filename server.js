3a68a5cf2083fe247761a8ee7b8f97cedf89338a
Johnlouiee <purisimajohnlouie@gmail.com>
2025-05-17 00:32:07 +0800

    try fixs

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/final-intprog')));
+// Serve static files from the Frontend/dist/final-intprog directory
+app.use(express.static(path.join(__dirname, 'Frontend/dist/final-intprog')));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/final-intprog/index.html'));
+  res.sendFile(path.join(__dirname, 'Frontend/dist/final-intprog/index.html'));
 });
 
 // Start the app by listening on the default port
