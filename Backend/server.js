47991d0e03c22962698a693172a940475c7aae0c
Johnlouiee <purisimajohnlouie@gmail.com>
2025-05-18 12:06:56 +0800

    Update cors

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,9 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  exposedHeaders: ['Set-Cookie'],
+  maxAge: 86400 // 24 hours
 }));
 
 // API routes
